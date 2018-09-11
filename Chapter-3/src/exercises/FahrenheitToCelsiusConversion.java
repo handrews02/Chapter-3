be77a0bd2494a3af6a7b2729d62c9d3820be939a
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-11 09:54:17 -0500

    updated text

---
+++ Chapter-3/src/exercises/FahrenheitToCelsiusConversion.java
@@ -0,0 +1,23 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class FahrenheitToCelsiusConversion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		double ftemp;
+		System.out.println("What temperature is it at 8 a.m.? ");
+		ftemp= input.nextDouble();
+		
+		displayinfo(ftemp);
+
+	}
+public static void displayinfo(double ftemp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
