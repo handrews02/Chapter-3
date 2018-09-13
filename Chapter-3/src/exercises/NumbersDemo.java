de02b5fd8fe59e19cb34572186c87d29484ba277
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-13 09:00:50 -0500

    updated text

---
+++ Chapter-3/src/exercises/NumbersDemo.java
@@ -0,0 +1,23 @@
+package exercises;
+
+public class NumbersDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int x = 5;
+			int y = 12;
+			
+			displayTwiceTheNumber(x, y); 
+
+	}
+public static void displayTwiceTheNumber(int x, int y) {
+	
+	int x2;
+	int y2;
+	
+	x2= x * 2;
+	y2= y * 2;
+	
+	System.out.println("x = " + x2 + " y = " + y2);
+	}
+}
