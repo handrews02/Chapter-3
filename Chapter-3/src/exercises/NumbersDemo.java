c04a3b7b183e2a82528abbdd155fef138f37b17d
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-13 09:08:15 -0500

    updated text

--- Chapter-3/src/exercises/NumbersDemo.java
+++ Chapter-3/src/exercises/NumbersDemo.java
@@ -8,6 +8,8 @@ public class NumbersDemo {
 			int y = 12;
 			
 			displayTwiceTheNumber(x, y); 
+			displayNumberPlusFive(x, y);
+			displayNumberSquared(x, y); 
 
 	}
 public static void displayTwiceTheNumber(int x, int y) {
@@ -19,5 +21,24 @@ public static void displayTwiceTheNumber(int x, int y) {
 	y2= y * 2;
 	
 	System.out.println("x = " + x2 + " y = " + y2);
+	}
+public static void displayNumberPlusFive(int x, int y) {
+	int x3;
+	int y3;
+	
+	x3= x + 5;
+	y3= y + 5;
+	
+	System.out.println("x = " + x3 + " y = " + y3); 
+	}
+public static void displayNumberSquared(int x, int y) {
+	
+	int x4;
+	int y4;
+	
+	x4= x * x;
+	y4= y * y;
+	
+	System.out.println("x = " + x4 + " y = " + y4); 
 	}
 }
