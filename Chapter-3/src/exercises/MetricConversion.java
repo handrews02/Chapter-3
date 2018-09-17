7ddec7d271c6b3dbac63308417d58dc31250a598
Hannah Andrews <handrews02@users.noreply.github.com>
2018-09-17 09:23:30 -0500

    updated text

--- Chapter-3/src/exercises/MetricConversion.java
+++ Chapter-3/src/exercises/MetricConversion.java
@@ -6,8 +6,7 @@ public class MetricConversion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		double centi= 2.54;
-		double liters= 3.7854;
+		
 		double inches;
 		double gallons;
 		Scanner input = new Scanner(System.in);
@@ -16,17 +15,19 @@ public class MetricConversion {
 		inches= input.nextDouble();
 		System.out.println("Please enter a number of gallons >> ");
 		gallons= input.nextDouble();
-		displayInchesConversion(inches, centi);
-		displayLitersConversion(gallons, liters);
+		displayInchesConversion(inches);
+		displayLitersConversion(gallons);
 		
 	}
-public static void displayInchesConversion (double inches, double centi) {
+public static void displayInchesConversion (double inches) {
 	double inchesConversion;
+	double centi= 2.54;
 	inchesConversion= inches * centi;
 	System.out.println("Your inches to centimeters conversion is " + inchesConversion + " centimeters");
 	}
-public static void displayLitersConversion (double gallons, double liters) {
+public static void displayLitersConversion (double gallons) {
 	double gallonsConversion;
+	double liters= 3.7854;
 	gallonsConversion= gallons * liters;
 	System.out.println("Your gallons to liters conversion is " + gallonsConversion + " liters");
 }
